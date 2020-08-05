88c80c992b36d47ce725705187ceb7123ed692bb
Juana Villalpando <juana.v14@gmail.com>
2020-08-05 14:25:20 -0700

    add a simple challenge

---
+++ TLcode/challenges.py
@@ -0,0 +1,16 @@
+"""
+Print out each element of the following array on a separate line:
+
+['Joe', 2, 'Ted', 4.98, 14, 'Sam', 'void *', '42', 'float', 'pointers', 5006]
+
+You may use whatever programming language you'd like.
+Verbalize your thought process as much as possible before writing any code. 
+Run through the UPER problem solving framework while going through your thought process.
+
+"""
+
+array_list = ['Joe', 2, 'Ted', 4.98, 14, 'Sam', 'void *', '42', 'float', 'pointers', 5006]
+#iterate through list
+for item in array_list:
+    #print each item indivudually
+    print(item)
